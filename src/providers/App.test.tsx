0544b6700259c6c13c93fb39f11d56a4873994c4
George Bodea <bodea.george12@yahoo.com>
2023-02-28 22:21:00 +0200

    added jest and ci/cd script for github actions

---
+++ src/providers/App.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react'
+import App from './App';
+
+it('should find the component associated with the text', () => {
+
+render(<App />);
+expect(screen.getByText(/hello/i)).toBeDefined();
+
+
+} )
