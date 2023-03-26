d3cef8b9ece35190bfd1b7952e122fb0c542e833
George Bodea <bodea.george12@yahoo.com>
2023-03-26 16:06:26 +0300

    added context and dark/light functionality

--- src/components/shared/Navbar/ActionGroup/actiongroup/GithubButton/GithubButton.tsx
+++ src/components/shared/Navbar/ActionGroup/actiongroup/GithubButton/GithubButton.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import GithubSvg from '@/assets/github-icon.svg';
+import GithubLightSVG from '@/assets/light/github.svg';
+import GithubDarkSVG from '@/assets/dark/github.svg';
 
 interface GithubButtonProps {
   className: string,
   size : number,
+  theme : string,
 }
 
-const GithubButton : React.FC<GithubButtonProps> = ({ className, size }) => (
-  <button className={className} type="button">
+const GithubButton : React.FC<GithubButtonProps> = ({ className, size, theme }) => (
 
-    <img src={GithubSvg} alt="Github" className={`w-${size} h-${size}`} />
+  <button className={className} type="button">
+    <a href="https://github.com/jorjdev/Recipes" target="_blank" rel="noopener noreferrer">
+      <img src={theme === 'dark' ? GithubDarkSVG : GithubLightSVG} alt="Github" className={`w-${size} h-${size}`} />
+    </a>
   </button>
+
 );
 
 export default GithubButton;
--- src/components/shared/Navbar/ActionGroup/actiongroup/SearchButton/SearchButton.tsx
+++ src/components/shared/Navbar/ActionGroup/actiongroup/SearchButton/SearchButton.tsx
@@ -4,12 +4,14 @@ import SearchIconSvg from '@/assets/search-icon.svg';
 interface SearchButtonProps {
   className: string,
   size : number,
+  theme : string,
 }
 
-const SearchButton : React.FC<SearchButtonProps> = ({ className, size }) => (
+const SearchButton : React.FC<SearchButtonProps> = ({ className, size, theme }) => (
+
   <button className={className} type="button">
 
-    <img src={SearchIconSvg} alt="Search" className={`w-${size} h-${size}`} />
+    <img src={theme === 'dark' ? SearchIconSvg : SearchIconSvg} alt="Search" className={`w-${size} h-${size}`} />
   </button>
 );
 
