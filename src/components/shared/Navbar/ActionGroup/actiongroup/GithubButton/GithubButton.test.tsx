a5d7203e94a765f2bbda6df844ef44adefd5c7c0
George Bodea <bodea.george12@yahoo.com>
2023-03-26 16:16:08 +0300

    build errors

--- src/components/shared/Navbar/ActionGroup/actiongroup/GithubButton/GithubButton.test.tsx
+++ src/components/shared/Navbar/ActionGroup/actiongroup/GithubButton/GithubButton.test.tsx
@@ -2,6 +2,6 @@ import { render, screen } from '@testing-library/react';
 import GithubButton from './GithubButton';
 
 it('should find the component associated with the text', () => {
-  render(<GithubButton className="" size={2} />);
+  render(<GithubButton className="" theme="" size={2} />);
   expect(screen.getByText(/search/i)).toBeDefined();
 });
